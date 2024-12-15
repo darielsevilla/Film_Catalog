33a8ccb6f345d46dd81ac47c1b0346c0a17cf7f8
Tatiana-Garcia <tzgarcia_16@unitec.edu>
2024-12-14 21:28:51 -0600

    "Actualizacion"

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Image, StyleSheet, Platform } from 'react-native';
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
@@ -23,6 +24,9 @@ import InfoPage from './infopage';
 import MainScreen from './mainscreen';
 import { AppState } from 'react-native';
 import axios from 'axios';
+//import { useEffect } from 'react';
+///import { AsyncStorage } from 'react-native';
+
 
 type RootStackParamList = {
   SearchingScreen: undefined;
@@ -38,50 +42,53 @@ type RootStackParamList = {
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 const templateScreen = () => {
-  return (<ParallaxScrollView
-    headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-    headerImage={
-      <Image
-        source={require('@/assets/images/partial-react-logo.png')}
-        style={styles.reactLogo}
-      />
-    }>
-    <ThemedView style={styles.titleContainer}>
-      <ThemedText type="title">Welcome!</ThemedText>
-      <HelloWave />
-    </ThemedView>
-    <ThemedView style={styles.stepContainer}>
-      <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-      <ThemedText>
-        Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText>s
-        Press{' '}
-        <ThemedText type="defaultSemiBold">
-          {Platform.select({
-            ios: 'cmd + d',
-            android: 'cmd + m',
-            web: 'F12'
-          })}
-        </ThemedText>{' '}
-        to open developer tools.
-      </ThemedText>
-    </ThemedView>
-    <ThemedView style={styles.stepContainer}>
-      <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-      <ThemedText>
-        Tap the Explore tab to learn more about what's included in this starter app.
-      </ThemedText>
-    </ThemedView>
-    <ThemedView style={styles.stepContainer}>
-      <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-      <ThemedText>
-        When you're ready, run{' '}
-        <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-        <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-        <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-        <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-      </ThemedText>
-    </ThemedView>
-  </ParallaxScrollView>);
+
+  return (
+
+    <ParallaxScrollView
+      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+      headerImage={
+        <Image
+          source={require('@/assets/images/partial-react-logo.png')}
+          style={styles.reactLogo}
+        />
+      }>
+      <ThemedView style={styles.titleContainer}>
+        <ThemedText type="title">Welcome!</ThemedText>
+        <HelloWave />
+      </ThemedView>
+      <ThemedView style={styles.stepContainer}>
+        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
+        <ThemedText>
+          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText>s
+          Press{' '}
+          <ThemedText type="defaultSemiBold">
+            {Platform.select({
+              ios: 'cmd + d',
+              android: 'cmd + m',
+              web: 'F12'
+            })}
+          </ThemedText>{' '}
+          to open developer tools.
+        </ThemedText>
+      </ThemedView>
+      <ThemedView style={styles.stepContainer}>
+        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
+        <ThemedText>
+          Tap the Explore tab to learn more about what's included in this starter app.
+        </ThemedText>
+      </ThemedView>
+      <ThemedView style={styles.stepContainer}>
+        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
+        <ThemedText>
+          When you're ready, run{' '}
+          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
+          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
+          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
+          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
+        </ThemedText>
+      </ThemedView>
+    </ParallaxScrollView>);
 }
 
 
